1b7446c7f98e817704ca23fa8fd17369b394e54b
Hai <vuhaix95@gmail.com>
2020-03-13 20:31:48 +0700

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `Hello cả nhà`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
