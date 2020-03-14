ae54ab28bd45a19d675d8d92577b4e7e704c44d9
Trịnh Văn Duy <49477836+Duytv081298@users.noreply.github.com>
2020-03-14 14:30:08 +0700

    add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `Hello cả nhà`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
