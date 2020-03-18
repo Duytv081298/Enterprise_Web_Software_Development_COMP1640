a5fb0895551f1c3f107db72d8db9a111c77f5495
Hai <vuhaix95@gmail.com>
2020-03-18 15:05:37 +0700

    Homepage ver1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,24 @@ import { LoginComponent } from './components/login/login.component';
 
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NewsComponent } from './components/news/news.component';
+import { ContentHomeComponent } from './components/content-home/content-home.component';
+import { MajorsComponent } from './components/majors/majors.component';
+import { ClubsComponent } from './components/clubs/clubs.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    NavBarComponent,
+    NewsComponent,
+    ContentHomeComponent,
+    MajorsComponent,
+    ClubsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
