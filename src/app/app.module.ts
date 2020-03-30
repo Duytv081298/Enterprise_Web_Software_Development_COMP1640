ba539e0ffac6bac3828a4b4ff6ac93d039a70bee
Hai <vuhaix95@gmail.com>
2020-03-30 23:08:19 +0700

    list-account and add-account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListAccountComponent } from './components/list-account/list-account.component';
+import { AddAccountComponent } from './components/add-account/add-account.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HeaderComponent,
+    ListAccountComponent,
+    AddAccountComponent,
   ],
   imports: [
     BrowserModule,
