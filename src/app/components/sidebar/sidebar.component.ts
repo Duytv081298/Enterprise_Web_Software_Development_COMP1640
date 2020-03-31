a247b2b736574b9545023c50cd8de3c355f1682a
Trịnh Văn Duy <49477836+Duytv081298@users.noreply.github.com>
2020-03-31 12:36:30 +0700

    select Account

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,6 +10,24 @@ export class SidebarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.CheckAccount();
+  }
+  @Input() typeAccount: String;
+  
+  accountStaff = null;
+  accountTutor = null;
+  accountStudent = null;
+
+  CheckAccount(){
+    if(this.typeAccount == 'Staff'){
+      this.accountStaff = 'Staff';
+
+    }else if(this.typeAccount == 'Tutor'){
+      this.accountTutor = 'Tutor';
+
+    }else if(this.typeAccount == 'Student') {
+      this.accountStudent = 'Student';
+    }
   }
 
 }
