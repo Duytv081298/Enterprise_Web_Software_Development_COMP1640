fba5502a3c9dc1eeb431c84be994763a256f7649
Hieu <Hieukhiqt2@gmail.com>
2020-04-02 22:29:35 +0700

    Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class AppComponent {
   title = 'Web-Development-COMP1640';
@@ -12,4 +13,14 @@ export class AppComponent {
   receiveMessage($event) {
     this.typeAccount = $event;
   }
+
+constructor(config: NgbModalConfig, private modalService: NgbModal) {
+  // customize default values of modals used by this component tree
+  config.backdrop = 'static';
+  config.keyboard = false;
+}
+
+open(content) {
+  this.modalService.open(content);
+}
 }
