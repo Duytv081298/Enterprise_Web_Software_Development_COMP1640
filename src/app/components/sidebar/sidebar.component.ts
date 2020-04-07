64516116ed3cf7c13422115d633bfde4a98889e1
Trịnh Văn Duy <49477836+Duytv081298@users.noreply.github.com>
2020-04-07 15:05:12 +0700

    login sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AccountClassificationService } from 'src/app/services/account-classification.service';
+import {LoginComponent} from '../login/login.component'
 
 @Component({
   selector: 'app-sidebar',
@@ -7,30 +7,31 @@ import { AccountClassificationService } from 'src/app/services/account-classific
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  account : String;
+  typeAccount: string;
 
-  constructor( private typeAccount : AccountClassificationService) { }
-
+  constructor( private loginComponent : LoginComponent ) { }
   ngOnInit(): void {
-    // this.CheckAccount();
-    console.log(this.account)
   }
-  
   accountStaff = null;
   accountTutor = null;
   accountStudent = null;
+  ngDoCheck() {
+    this.CheckAccount();
+  }
+  CheckAccount(){
+    this.setTypeUser();
+    if(this.typeAccount == 'staff'){
+      this.accountStaff = 'staff';
 
-  // CheckAccount(){
-  //   this.typeAccount.share.subscribe(x => this.account = x)
-  //   if(this.account == 'staff'){
-  //     this.accountStaff = 'staff';
+    }else if(this.typeAccount == 'tutor'){
+      this.accountTutor = 'tutor';
 
-  //   }else if(this.account == 'tutor'){
-  //     this.accountTutor = 'tutor';
-
-  //   }else if(this.account == 'student') {
-  //     this.accountStudent = 'student';
-  //   }
-  // }
+    }else if(this.typeAccount == 'student') {
+      this.accountStudent = 'student';
+    }
+  }
+  setTypeUser(){
+    this.typeAccount = this.loginComponent.getTypeUser()
+  }
 
 }
