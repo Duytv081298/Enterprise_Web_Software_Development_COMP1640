5f41d2815a7aae5c14a621aa19d0768ecd6e3e6e
Hieu <Hieukhiqt2@gmail.com>
2020-04-12 17:29:53 +0700

    fix reset page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,8 +30,8 @@ export class AppComponent implements OnInit {
 
   }
   ngDoCheck() {
+    this.user = this.etutoringService.setUser()
     this.showLogin()
-    // this.receiveData()
   }
   
   showLogin(){
