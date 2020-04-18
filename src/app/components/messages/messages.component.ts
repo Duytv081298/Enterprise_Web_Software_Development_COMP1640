2fa8a75ed80d810aa3f1e102015042caf939c62a
Trịnh Văn Duy <49477836+Duytv081298@users.noreply.github.com>
2020-04-18 21:25:23 +0700

    revert

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,48 +1,15 @@
-import { Component } from '@angular/core';
-import { ChatService } from 'src/chat.service';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css'],
-  providers:[ChatService]
+  styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent{
+export class MessagesComponent implements OnInit {
 
-  user:String;
-  room:String;
-  messageText:String;
-  messageArray:Array<{user:String,message:String}> = [];
-  constructor(private _chatService:ChatService){
-      this._chatService.newUserJoined()
-      .subscribe(data=> this.messageArray.push(data));
+  constructor() { }
 
-
-      this._chatService.userLeftRoom()
-      .subscribe(data=>this.messageArray.push(data));
-
-      this._chatService.newMessageReceived()
-      .subscribe(data=>this.messageArray.push(data));
-
-
+  ngOnInit(): void {
   }
-
-  join(){
-      this._chatService.joinRoom({user:this.user, room:this.room});
-  }
-
-  leave(){
-      this._chatService.leaveRoom({user:this.user, room:this.room});
-  }
-
-  sendMessage()
-  {
-      console.log(this.messageArray)
-
-      this._chatService.sendMessage({user:this.user, room:this.room, message:this.messageText});
-  }
-
 
 }
-
