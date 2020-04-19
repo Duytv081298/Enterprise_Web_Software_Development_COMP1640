6e0f56cac39d1c07ccff558c62fadbf47c424445
Hai <vuhaix95@gmail.com>
2020-04-19 14:50:27 +0700

    Chat-app ver3

--- Web_Development_COMP1640/src/app/components/messages/messages.component.ts
+++ Web_Development_COMP1640/src/app/components/messages/messages.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { ChatService } from 'src/chat.service';
+
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
+  providers:[ChatService]
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent {
+  user:String;
+  room:String;
+  messageText:String;
+  messageArray:Array<{user:String,message:String}> = [];
+  constructor(private _chatService:ChatService){
+      this._chatService.newUserJoined()
+      .subscribe(data=> this.messageArray.push(data));
 
-  constructor() { }
 
-  ngOnInit(): void {
+      this._chatService.userLeftRoom()
+      .subscribe(data=>this.messageArray.push(data));
+
+      this._chatService.newMessageReceived()
+      .subscribe(data=>this.messageArray.push(data));
+
+
   }
+
+  join(){
+      this._chatService.joinRoom({user:this.user, room:this.room});
+  }
+
+  leave(){
+      this._chatService.leaveRoom({user:this.user, room:this.room});
+  }
+
+  sendMessage()
+  {
+      console.log(this.messageArray)
+
+      this._chatService.sendMessage({user:this.user, room:this.room, message:this.messageText});
+  }
+
 
 }
