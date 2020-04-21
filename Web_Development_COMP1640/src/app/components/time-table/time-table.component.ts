2764898f30764f6551f6d09099c4c1b545941370
Hieu <Hieukhiqt2@gmail.com>
2020-04-21 16:35:10 +0700

    timetable v2

--- Web_Development_COMP1640/src/app/components/time-table/time-table.component.ts
+++ Web_Development_COMP1640/src/app/components/time-table/time-table.component.ts
@@ -12,10 +12,13 @@ import { Student } from 'src/app/models/student';
   templateUrl: './time-table.component.html',
   styleUrls: ['./time-table.component.css']
 })
+
 export class TimeTableComponent implements OnInit {
   user: User = this.loginComponent.getUser()
   @Input() tutor: Tutor
   @Input() student: Student
+  fromDate: string
+  toDate: string
   slots: Slot[] = []
   timeOuts = ['Slot 1 (7:30-9:00)', 'Slot 2 (9:10-10:40)', 'Slot 3 (10:50-12:20)', 'Slot 4 (12:50-14:20)',
     'Slot 5 (14:30-16:00)', 'Slot 6 (16:10-17:40)', 'Slot 7 (17:50-19:20)', 'Slot 8 (19:30-21:00)']
@@ -27,50 +30,99 @@ export class TimeTableComponent implements OnInit {
   day6: boolean;
   day7: boolean;
 
-  weeks = this.getWeek()
+  weekUp = this.getWeekOne().reverse()
+  weekDown = this.getWeek()
+  weeks = this.weekDown.concat(this.weekUp)
+  
   slotArrays = [[]]
   schedules: Schedule;
 
   constructor(private etutoringService: EtutoringService,
     private loginComponent: LoginComponent) {
   }
-
   ngOnInit(): void {
-    this.receiveData()
-    this.getSchedule()
-  
+    this.receiveData(this.fromDate,this.toDate)
+    this.getSchedule(this.fromDate,this.toDate)
   }
+  getWeekOne(): string[]{
+      let cur = new Date();
+      let firstUp = cur.getDate() - cur.getDay();
+      let curMonth = cur.getMonth();
+      let beforeMonth = cur.getMonth();
+      let weekUp = []
 
+      for(let i = 1; i<= 10; i++){
+        let afterFirsts = firstUp + 7*i
+        if(afterFirsts < 0){
+            firstUp -= ( new Date(cur.getFullYear(), beforeMonth ,0).getDate())
+            curMonth = curMonth + 1
+            afterFirsts = firstUp + 7*i
+          }
+          var up = new Date();
+          up.setMonth(curMonth)
+          up.setDate(afterFirsts)
+          up.toUTCString()
+          let bindFirstDayUp = this.formatDate(up)
+          let afterLasts = afterFirsts + 6
+          let curMonthAfter = curMonth
+
+          if(afterFirsts < new Date(cur.getFullYear(), curMonth ,0).getDate()){
+            curMonthAfter = curMonth + 1
+            afterLasts -= ( new Date(cur.getFullYear(), curMonth + 1,0).getDate())
+          }
+          var afterDayLast = new Date();
+          afterDayLast.setMonth(curMonthAfter)
+          afterDayLast.setDate(afterLasts)
+          afterDayLast.toUTCString()
+          let bindLastDayUp = this.formatDate(afterDayLast)
+          weekUp.splice(0, 0, bindFirstDayUp + ' to ' + bindLastDayUp)   
+      }
+      return weekUp
+  }
   getWeek(): string[] {
-
+    
     let curr = new Date();
-
     let first = curr.getDate() - curr.getDay();
+    let currMonth = curr.getMonth();
+    
     let firstday = this.formatDate(new Date(curr.setDate(first)).toUTCString())
+    this.fromDate = firstday
     let last = first + 6;
     let lastday = this.formatDate(new Date(curr.setDate(last)).toUTCString())
+    this.toDate = lastday
     let weeks = [firstday + ' to ' + lastday]
 
     for (let i = 1; i <= 10; i++) {
+      
+      let behindFirsts = first - 7*i
+      if(behindFirsts < 0) {
+        first = first + new Date(curr.getFullYear(), currMonth ,0).getDate() 
+        currMonth = currMonth - 1
+        behindFirsts = first - 7*i
+      }
+      var d = new Date();
+      d.setMonth(currMonth)
+      d.setDate(behindFirsts)
+      d.toUTCString()
+      let bindFirstDays = this.formatDate(d)
+      let behindLasts = behindFirsts + 6
+      let currMonthLast = currMonth
 
-      let beforeFirsts = first + 7 * i
-      let beforeFirstDays = this.formatDate(new Date(curr.setDate(beforeFirsts)).toUTCString())
-
-      let beforeLasts = beforeFirsts + 6
-      let beforeLastDays = this.formatDate(new Date(curr.setDate(beforeLasts)).toUTCString())
-      weeks.push(beforeFirstDays + ' to ' + beforeLastDays)
-
-      let behindFirsts = first - 7 * i
-
-      let bindFirstDays = this.formatDate(new Date(curr.setDate(behindFirsts)).toUTCString())
-      let behindLasts = behindFirsts - 6
-      let bindLastDays = this.formatDate(new Date(curr.setDate(behindLasts)).toUTCString())
-
+      if(behindLasts > new Date(curr.getFullYear(), currMonth ,0).getDate()){
+        currMonthLast = currMonth + 1
+        behindLasts -=  ( new Date(curr.getFullYear(), currMonth + 1 ,0).getDate())
+      }
+      var behindDayLast = new Date();
+      behindDayLast.setMonth(currMonthLast)
+      behindDayLast.setDate(behindLasts)
+      behindDayLast.toUTCString()
+      let bindLastDays = this.formatDate(behindDayLast)
       weeks.splice(0, 0, bindFirstDays + ' to ' + bindLastDays)
     }
     return weeks
   }
-  receiveData() {
+  
+  receiveData(fromDate,toDate) {
     this.user = this.loginComponent.getUser()
 
     if (this.user != null) {
@@ -82,17 +134,17 @@ export class TimeTableComponent implements OnInit {
       } else if (this.user.type == 'tutor') {
         this.etutoringService.getTutor(this.loginComponent.getUser().username).subscribe(
           data => {
-            this.etutoringService.getSchedule('tutorId', data.id).subscribe(
+            this.etutoringService.getSchedule('tutorId', data.id,fromDate,toDate).subscribe(
               data => {
                 this.schedules = data
                 this.addSlot(data)
               }
             )
           })
-      } else if (this.user.type == 'student') {
+      } else if (this.user.type == 'student'){
         this.etutoringService.getStudent(this.loginComponent.getUser().username).subscribe(
           data => {
-            this.etutoringService.getSchedule('studentId', data.id).subscribe(
+            this.etutoringService.getSchedule('studentId', data.id,fromDate,toDate).subscribe(
               data => {
                 this.schedules = data
                 this.addSlot(data)
@@ -101,16 +153,16 @@ export class TimeTableComponent implements OnInit {
       }
     }
   }
-  getSchedule() {
+  getSchedule(fromDate, toDate) {
     if (this.tutor != null) {
-      this.etutoringService.getSchedule('tutorId', this.tutor.id).subscribe(
+      this.etutoringService.getSchedule('tutorId', this.tutor.id,fromDate, toDate).subscribe(
         data => {
           this.schedules = data
           this.addSlot(data)
         }
       )
     } if (this.student != null) {
-      this.etutoringService.getSchedule('studentId', this.student.id).subscribe(
+      this.etutoringService.getSchedule('studentId', this.student.id, fromDate, toDate).subscribe(
         data => {
           this.schedules = data
           this.addSlot(data)
@@ -119,39 +171,46 @@ export class TimeTableComponent implements OnInit {
     }
   }
   addSlot(data) {
-    data.calendarDtoList.forEach(element => {
-      var resultArray = Object.keys(element).map(function (personNamedIndex) {
-        let person = element[personNamedIndex];
+    
+    this.slots = []
+    this.slotArrays = [[]]
+    data.forEach(element => {
+      let arrChild = []
+      var resultArray = Object.keys(element).map(function (b) {
+        let person = element[b];
+        var array = Object.keys(person).map(function (c) {
+          let p = person[c];
+          return p;
+        });
+        arrChild.push(array)
         return person;
       });
-      this.slotArrays.push(resultArray)
+      this.slotArrays.push(arrChild)
     });
-    this.slotArrays = this.slotArrays.splice(1)
+      this.slotArrays.splice(0,1)
     for (let i = 1; i <= 8; i++) {
       for (let j = 0; j <= 6; j++) {
         if (j == 0) {
-          this.day1 = this.slotArrays[j][i]
+          this.day1 = this.slotArrays[j][i][1]
         } else if (j == 1) {
-          this.day2 = this.slotArrays[j][i]
+          this.day2 = this.slotArrays[j][i][1]
         } else if (j == 2) {
-          this.day3 = this.slotArrays[j][i]
+          this.day3 = this.slotArrays[j][i][1]
         } else if (j == 3) {
-          this.day4 = this.slotArrays[j][i]
+          this.day4 = this.slotArrays[j][i][1]
         } else if (j == 4) {
-          this.day5 = this.slotArrays[j][i]
-        }
-        else if (j == 5) {
-          this.day6 = this.slotArrays[j][i]
-        }
-        else {
-          this.day7 = this.slotArrays[j][i]
+          this.day5 = this.slotArrays[j][i][1]
+        } else if (j == 5) {
+          this.day6 = this.slotArrays[j][i][1]
+        } else {
+          this.day7 = this.slotArrays[j][i][1]
         }
       }
       let slot: Slot = {
         timeout: this.timeOuts[i - 1],
         day1: this.day1, day2: this.day2, day3: this.day3, day4: this.day4, day5: this.day5, day6: this.day6, day7: this.day7
       }
-      this.slots.push(slot)
+      this.slots.push(slot) 
     }
   }
   formatDate(date) {
@@ -168,7 +227,11 @@ export class TimeTableComponent implements OnInit {
   }
 
   selectWeek(selectWeek) {
-    let startDate = selectWeek.substring(0, 10)
-    let endDate = selectWeek.slice(-10)
+    this.fromDate = selectWeek.substring(0, 10)
+    this.toDate = selectWeek.slice(-10)
+    console.log(this.fromDate)
+    console.log(this.toDate)
+    this.receiveData(selectWeek.substring(0, 10),selectWeek.slice(-10))
+    this.getSchedule(selectWeek.substring(0, 10),selectWeek.slice(-10))
   }
 }
